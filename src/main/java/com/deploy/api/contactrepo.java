5572e1a769865e04612a6d322e03310aca426f57
sarvesh <02sarveshb@gmail.com>
2024-07-24 15:08:18 +0530

    changed

--- src/main/java/com/deploy/api/contactrepo.java
+++ src/main/java/com/deploy/api/contactrepo.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface contactrepo extends JpaRepository<contact,Integer>{
@@ -15,6 +17,8 @@ public interface contactrepo extends JpaRepository<contact,Integer>{
 	@Query("from contact as c where c.User.id = :userId")
 	public Page<contact>findallcontacts(@Param("userId")int userId,Pageable pageable);
 
+	@Transactional
+	@Modifying
 	@Query(value = "delete from contact as c where c.cId = :cId")
       public contact deletecontact(@Param("cId")int cId);
 	
